1885f9b72af4bdf86d96729429efc80c0ef39bed
alireza <farmanialireza668@gmail.com>
2025-04-25 11:12:17 +0330

    set project colors

--- lib/core/theme/color.dart
+++ lib/core/theme/color.dart
@@ -1,3 +1,44 @@
+import 'package:flutter/widgets.dart';
+
 class ColorApp {
-  // primary color
+  // primary colors
+  static const Color primary = Color(0xff417F56);
+  static const Color shade_1 = Color(0xff396F4B);
+  static const Color shade_2 = Color(0xff315F41);
+  static const Color shade_3 = Color(0xff294F36);
+  static const Color shade_4 = Color(0xff21402B);
+  static const Color shade_5 = Color(0xff183020);
+  static const Color shade_6 = Color(0xff102016);
+  static const Color shade_7 = Color(0xff08100B);
+  // tint color
+  static const Color tint_1 = Color(0xffE5F2E9);
+  static const Color tint_2 = Color(0xffCAE4D3);
+  static const Color tint_3 = Color(0xffB0D7BD);
+  static const Color tint_4 = Color(0xff96C9A7);
+  static const Color tint_5 = Color(0xff7CBC91);
+  static const Color tint_6 = Color(0xff61AE7B);
+  static const Color tint_7 = Color(0xff4E9968);
+  // neutral colors
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff0C0C0C);
+  static const Color gray_1 = Color(0xffF9F9F9);
+  static const Color gray_2 = Color(0xffF9F9F9);
+  static const Color gray_3 = Color(0xffEDEDED);
+  static const Color gray_4 = Color(0xffCBCBCB);
+  static const Color gray_5 = Color(0xffCBCBCB);
+  static const Color gray_6 = Color(0xffADADAD);
+  static const Color gray_7 = Color(0xff757575);
+  static const Color gray_8 = Color(0xff353535);
+  // status colors
+  static const Color error = Color(0xffC30000);
+  static const Color errorLight = Color(0xffED2E2E);
+  static const Color errorExtralight = Color(0xffFFF2F2);
+  //success colors
+  static const Color success = Color(0xff00966D);
+  static const Color successLight = Color(0xff00BA88);
+  static const Color successExtralight = Color(0xffF3FDFA);
+  // warning colors
+  static const Color warning = Color(0xffA9791C);
+  static const Color warningLight = Color(0xffF4B740);
+  static const Color warningExtralight = Color(0xffF3FDFA);
 }
--- lib/screens/splash/splash_screens.dart
+++ lib/screens/splash/splash_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tarkhineh_app/core/theme/color.dart';
 
 class SplashScreens extends StatefulWidget {
   const SplashScreens({super.key});
@@ -10,6 +11,25 @@ class SplashScreens extends StatefulWidget {
 class _SplashScreensState extends State<SplashScreens> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(decoration: BoxDecoration(
+              color: ColorApp.primary
+            )),
+            Container(
+              child: Text("hello world", style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold
+              ),),
+            ),
+            Container(),
+          ],
+        ),
+      ),
+    );
   }
 }
